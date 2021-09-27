0765253018d4389cf02027c0a1b7524e8d95b141
andrey14scr <70211433+andrey14scr@users.noreply.github.com>
2021-09-27 12:39:36 +0300

    Added empty text controller.

---
+++ ComputationalLinguistics/Controllers/TextsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ComputationalLinguistics.Controllers
+{
+    public class TextsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
